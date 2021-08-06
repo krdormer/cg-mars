d691586abbb4cf24f3daa656b36f1dee9c95db89
krdormer <krdormer_dev@protonmail.com>
2021-08-06 18:07:59 -0500

    Render content in VideoWorkouts, add styles
    Updated VideoWorkouts page to dynamically render workout content using
    the withRouter HOC, and the match property. Currently it will assign th
    e URL parameter of workoutId to a constant called trainerId, which was
    sent from the Dashboard WorkoutLinkTag push and will be an integer. Thi
    s number is used to access a particular object in the workouts.json fil
    e, giving access to the media and other properties within. Based on the
    se properties, the title and the video stream can be rendered.Appears t
    o be working as expected for each button on the Dashboard page.

--- src/pages/VideoWorkouts/VideoWorkouts.js
+++ src/pages/VideoWorkouts/VideoWorkouts.js
@@ -1,9 +1,20 @@
-const VideoWorkouts = () => {
+import { withRouter } from 'react-router-dom';
+import data from '../../data/workouts.json';
+
+import './VideoWorkouts.styles.css';
+
+const VideoWorkouts = ({ match }) => {
+  const trainerId = match.params.workoutId;
+  const workoutData = data[trainerId - 1];
   return (
     <div className="video-workout-page">
-      <h1>I'm a page for workouts</h1>
+      <h2 className="workout-title">{workoutData.title}</h2>
+      <video className="workout-video-feed" controls>
+        <source src={workoutData.media} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
 
-export default VideoWorkouts;
+export default withRouter(VideoWorkouts);
