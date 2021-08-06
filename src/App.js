e37582338c7b7fa751cd0962e68dcea0fd7c171e
krdormer <krdormer_dev@protonmail.com>
2021-08-06 12:58:15 -0500

    Add Global CG CSS Lib, add Header component
    Added a Header component to App.js. Will serve as page branding by disp
    laying the CG logo, as well as any navigational links if needed. Added
    CDN to index.html, but unfortunately was not connecting as expected. In
    stead I went ahead and took the minified CSS file and added it to the i
    ndex.css file. Logo and styling now appear to be applying correctly.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Header from './components/Header/Header';
 import Dashboard from './pages/Dashboard/Dashboard';
-import VideoWorkouts from './pages/VideoWorkouts/VideoWorkouts';
+// import VideoWorkouts from './pages/VideoWorkouts/VideoWorkouts';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
-      <VideoWorkouts />
+      {/* <VideoWorkouts /> */}
     </div>
   );
 };
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <a
+        href="https://campgladiator.com/online"
+        className="logo logo--cg-full-color-black"
+        style={{ height: '60px', margin: '24px' }}
+      >
+        Camp Gladiator
+      </a>
+    </header>
+  );
+};
+
+export default Header;
