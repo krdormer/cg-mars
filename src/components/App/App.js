c947bd471ab92acc5f430bc19b2ebba773f62309
krdormer <krdormer_dev@protonmail.com>
2021-08-06 13:17:15 -0500

    Add react-router-dom package, update App.js
    Added the react-router-dom (RRD) package to take advantage of browser h
    istory API, and allow navigation through the SPA. Converted App.js to b
    e the main router component, changing the displayed page based on curre
    nt URL path. Currently renders Dashboard at root '/', and a simple work
    out page at '/workouts'. Will update to take dynamic URLs shortly.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Header from '../Header/Header';
 import Dashboard from '../../pages/Dashboard/Dashboard';
-// import VideoWorkouts from './pages/VideoWorkouts/VideoWorkouts';
+import VideoWorkouts from '../../pages/VideoWorkouts/VideoWorkouts';
+
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Dashboard />
-      {/* <VideoWorkouts /> */}
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        {/* Will need to update path to accept dynamic URL */}
+        <Route path="/workouts" component={VideoWorkouts} />
+      </Switch>
     </div>
   );
 };
